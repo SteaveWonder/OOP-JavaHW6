c3de99466b3391887ce99f9ae9ea233e4bd9ec6f
Stepan <soulsteff@gmail.com>
2024-03-05 13:20:04 +0300

    del

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        //TeacherService teacherService = new TeacherService();
+        //TeacherView teacherView = new TeacherView();
+        //TeacherController teacherController = new TeacherController(teacherService, teacherView);
+    }
+}
